dcac095a806709e8c9b748df521f63028d9271f8
awu <wufire@gmail.com>
2018-06-04 05:53:23 -0500

    Adding network call to iOS client. Base URL should be edited to your server instance.

--- RatesScheduleClient/RatesScheduleClient/ViewController.swift
+++ RatesScheduleClient/RatesScheduleClient/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Change this base URL to your RatesSchedule server instance
+    private let baseUrl = URL(string: "http://localhost:5000/")!
+    
     @IBOutlet weak var startTimeTextField: UITextField!
     @IBOutlet weak var endTimeTextField: UITextField!
     
@@ -60,6 +64,24 @@ class ViewController: UIViewController {
         self.view.endEditing(true)
     }
 
+    @IBAction func submitDates(_ sender: Any) {
+        if (validateDates()) {
+            let jsonObject: [String: String] = [
+                "starttime": formatDate(date: startTime),
+                "endtime": formatDate(date: endTime)
+            ]
+            do {
+                let dateData = try JSONSerialization.data(withJSONObject: jsonObject, options: JSONSerialization.WritingOptions())
+                getPrice(jsonData: dateData)
+            }
+            catch {
+                showSimpleAlert(title: "Parsing Error", message: "Encountered an error formatting data for the server. This doesn't really help you much, enduser")
+            }
+        } else {
+            showSimpleAlert(title: "Invalid Times", message: "Your requested times are invalid. Please correct them and try again.")
+        }
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -92,15 +114,30 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func submitDates(_ sender: Any) {
-        if (validateDates()) {
-            let alert = UIAlertController(title: "Price", message: "Fake Price", preferredStyle:.alert)
-            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        } else {
-            let alert = UIAlertController(title: "Invalid Times", message: "Your start and end times are invalid", preferredStyle:.alert)
-            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+    func showSimpleAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle:.alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func getPrice(jsonData: Data) {
+        let url = baseUrl.appendingPathComponent("api/rates/rateprice")
+        
+        if !jsonData.isEmpty {
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = jsonData
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let task = URLSession.shared.dataTask(with: request, completionHandler:
+                {
+                    (responseData: Data?, response: URLResponse?, error: Error?) in
+                    let responseString = String(data: responseData!, encoding: String.Encoding.utf8) as String?
+                    
+                    self.showSimpleAlert(title: "Price", message: "The price for the selected time is \(responseString ?? "not found").")
+                }
+            )
+            task.resume()
         }
     }
 }
